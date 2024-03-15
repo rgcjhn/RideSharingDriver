0ef9438ec0b210d3c613da2f81629898ebe2e9ec
Rogec John Pecarana <rjpecarana@gmail.com>
2024-03-15 09:35:39 +0800

    chore: setup redux for rides and users

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+
+import ridesReducer from "./rides/reducer";
+import usersReducer from "./users/reducer";
+
+const combinedReducers = combineReducers({
+  rides: ridesReducer,
+  users: usersReducer,
+});
+
+export const store = configureStore({
+  reducer: combinedReducers,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
