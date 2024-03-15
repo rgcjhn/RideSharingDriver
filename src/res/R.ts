f6b123daf0e5109e3de6bf9a11ff562e2b602872
Rogec John Pecarana <rjpecarana@gmail.com>
2024-03-15 12:15:16 +0800

    chore: global resource values

---
+++ src/res/R.ts
@@ -0,0 +1,21 @@
+const colors = {
+  primary: "#f3bc4c",
+  secondary: "#526cc1",
+  normal: "#212121",
+  faded: "#555555",
+  white: "#fff",
+  black: "#000",
+  invisible: "transparent",
+  pending: "#F1C231",
+  accepted: "#EEEEEE",
+  declined: "#CC0100",
+  started: "#F1C231",
+  "picked-up": "#526cc1",
+  "dropped-off": "#6AA74F",
+};
+
+const R = {
+  colors,
+};
+
+export default R;
